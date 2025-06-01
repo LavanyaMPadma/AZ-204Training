1888c777c91bc304d93ace53f9aba77e50c2fea3
Lavanya <lavanyamp1990@gmail.com>
2025-06-01 10:17:53 +0530

    Azure App Insights Demo

---
+++ 05-functions-code.cs
@@ -0,0 +1,14 @@
+﻿#r "Newtonsoft.Json"
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System.Net;
+using System.Threading.Tasks;
+
+public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
+{
+	log.LogInformation("C# HTTP trigger function processed a request.");
+
+	return new OkObjectResult("Hello Azure!");
+}
---
+++ 06-functions-queue-code.cs
@@ -0,0 +1,25 @@
+﻿#r "Newtonsoft.Json"
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+public static async Task<IActionResult> Run(
+	HttpRequest req,
+	IAsyncCollector<string> queueItem,
+	Stream blob,
+	ILogger log)
+{
+	log.LogInformation("C# HTTP trigger function processed a request.");
+
+	var message = "Hello Azure!";
+	await queueItem.AddAsync(message);
+	StreamWriter sr = new StreamWriter(blob);
+	await sr.WriteAsync(message);
+	sr.Flush();
+
+	return new OkObjectResult(message);
+}
